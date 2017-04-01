d3ed1637311c4a8e97abf07eb224920f624e429b
FreeStraint <rwu4@ualberta.ca>
2017-04-01 10:46:20 -0600

    first try, able to select equal amount of strength, still need to sort

--- a4.pl
+++ a4.pl
@@ -25,5 +25,61 @@ fourSquares(+N, [-S1, -S2, -S3, -S4])
 fourSquares(N, [S1, S2, S3, S4]):- [S1,S2,S3,S4] ins 0..N,
 									S1 #=< S2, S2 #=< S3, S3 #=< S4,
 									S1 * S1 + S2 * S2 + S3 * S3 + S4 * S4 #= N,
-									label([S1,S2,S3,S4]).									 
+									label([S1,S2,S3,S4]).
+
+/**
+Question 2 - War and Peace
+
+Two countries have signed a peace treaty and want to disarm over a period of months, 
+but they still don't completely trust each other. 
+Each month one of the countries can choose to dismantle one military division 
+while the other can dismantle two. 
+Each division has a certain strength, and both sides want to make sure that the total military strength 
+remains equal at each point during the disarmament process. 
+
+Country A: 1, 3, 3, 4, 6, 10, 12
+Country B: 3, 4, 7, 9, 16
+
+Month 1: A dismantles 1 and 3, B dismantles 4
+Month 2: A dismantles 3 and 4, B dismantles 7
+Month 3: A dismantles 12, B dismantles 3 and 9
+Month 4: A dismantles 6 and 10, B dismantles 16
+
+Begin:
+first check whether the two division sum is equal
+
+then start disarm
+disarmSelect will first select two element from Adivisions and select one elemnt from Bdivisions
+If found, continue on rest of the list
+If not found, then select one element from Adivisions and select two elements from Bdivisions
+If not found, return false
+*/									 
+
+disarm(Adivisions, Bdivisions, Solution):- sum(Adivisions, X), sum(Bdivisions, X), disarmSelect(Adivisions, Bdivisions, Solution).
+
+disarmSelect(Adivisions, Bdivisions, Solution):-
+				Vars = [A,B,C],
+				select(A, Adivisions, A1),
+				select(B, A1, A2),
+				select(C, Bdivisions, B1),
+				A #=< B, A + B #= C,
+				disarmSelect(A2,B1,S1),
+				append([[[A,B],C]], S1, Solution),
+				label(Vars).
+
+disarmSelect(Adivisions, Bdivisions, Solution):-
+				Vars = [A,B,C],
+				select(A, Adivisions, A1),
+				select(B, Bdivisions, B1),
+				select(C, B1, B2),
+				B #< C, B + C #= A,
+				disarmSelect(A1, B2, S1),
+				append([[A,[B,C]]], S1, Solution),
+				label(Vars).
+disarmSelect([],[],[]).
+
+sum([], 0).
+sum([A|L], X):- sum(L, X1), X is A + X1.
+
+
 
